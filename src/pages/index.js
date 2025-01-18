e9d9559e85d8cd9f0c28c335b6ff5d2bb69f157f
Muhammad Aoun Shabbir <aounshabbir432@gmail.com>
2025-01-18 21:38:35 +0500

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import {
   About, Blog, Contacts, Education,
-  Experience, Landing, Navbar, Projects, Skills
+  Experience, Landing, Navbar, Projects, Skills 
 } from '../components';
 import BackToTop from '../components/back-to-top/back-to-top';
 import ChangeTheme from '../components/change-theme/change-theme';
